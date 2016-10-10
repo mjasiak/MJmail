bd3e12c4781ea73f570f27979dd121504eba03dc
Jasiecki <mjasiak@pl.sii.eu>
2016-10-10 14:57:27 +0200

    Poszło

---
+++ MJmail/Data/MaildbContext.cs
@@ -0,0 +1,14 @@
+﻿using MJmail.Models;
+using System.Data.Entity;
+
+namespace MJmail.Data
+{
+    public class MaildbContext: DbContext
+    {
+        public MaildbContext() : base("DbContext")
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ MJmail/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MJmail.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+
+        public string MailTitle { get; set; }
+
+        public string MailContent { get; set; }
+
+        public string MailFrom { get; set; }
+
+        public string MailTo { get; set; }
+
+        public string MailDate { get; set; }
+    }
+}
