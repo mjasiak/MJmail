204df0142cb3220cce6f462ba1e3524887aabf22
Jasiecki <mjasiak@pl.sii.eu>
2016-10-25 09:39:35 +0200

    Chat position set, but not visible

---
+++ MJmail/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MJmail.Controllers
+{
+    public class ChatController : Controller
+    {
+    }
+}
