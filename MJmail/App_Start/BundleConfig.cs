16cb0e929b6515f7c2227f8cbb6879180f68a891
Jasiecki <mjasiak@pl.sii.eu>
2016-10-27 09:44:15 +0200

    Style refactory

--- MJmail/App_Start/BundleConfig.cs
+++ MJmail/App_Start/BundleConfig.cs
@@ -9,23 +9,23 @@ namespace MJmail
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Style/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Style/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Style/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Style/Scripts/bootstrap.js",
+                      "~/Style/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Style/Content/bootstrap.css",
+                      "~/Style/Content/site.css"));
         }
     }
 }
