9297588ea5af7b6c2de5aa4d464f8833e42c58fa
Jasiecki <mjasiak@pl.sii.eu>
2016-11-28 11:54:28 +0100

    Working chat&identity partial class <- Reason was that OWIN not startup two classes, but only one

---
+++ MJmail/App_Start/Startup.SignalRConfig.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace MJmail
+{
+    public partial class Startup
+    {
+        public void ConfigureSignalR(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
